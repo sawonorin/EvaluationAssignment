a118d7e99ae33ca2548d3d56d1a8ca0ca08a4803
Marcus Nordquist <marcus@queenslab.se>
2019-11-25 16:34:31 +0100

    ♠ Added Sample project

---
+++ AppendedProject/Models/DailyPattern.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace AvCalc.Models
+{
+    [Serializable]
+    public class DailyPattern
+    {
+        public virtual long Id { get; set; }
+        public virtual int ColorId { get; set; }
+        public virtual int PatternId { get; set; }
+        public virtual DateTime ValidTo { get; set; }
+    }
+
+    public enum ColorThemeNames
+    {
+        Red = 0,
+        Orange = 1,
+        Yellow = 2,
+        Green = 3,
+        LightBlue = 4,
+        Purple = 5,
+        Indigo = 6,
+        Steel = 7,
+        Pink = 8,
+        Blue = 9
+    }
+
+    public enum PatternThemes
+    {
+        City = 0,
+        Country = 1
+    }
+}
